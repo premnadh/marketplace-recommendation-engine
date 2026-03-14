a7b10bc6b5b5ef295a4d493b14b0aba50bbf464e
Gajula Premnadh <prem@Gajulas-MacBook-Air.local>
2026-03-14 10:42:53 +0000

    remove surprise dependency and use sklearn SVD

--- src/collaborative_filtering.py
+++ src/collaborative_filtering.py
@@ -1,34 +1,41 @@
 import pandas as pd
-from surprise import Dataset, Reader, SVD
+from sklearn.decomposition import TruncatedSVD
 
 
 class CollaborativeFilteringEngine:
 
     def __init__(self, interactions_path):
+        print("Loading interaction data...")
 
-        print("Loading interaction dataset...")
+        interactions = pd.read_csv(interactions_path)
 
-        df = pd.read_csv(interactions_path)
-
-        reader = Reader(rating_scale=(1, 5))
-
-        data = Dataset.load_from_df(
-            df[['user_id', 'product_id', 'rating']],
-            reader
+        user_item_matrix = interactions.pivot_table(
+            index="user_id",
+            columns="item_id",
+            values="rating",
+            fill_value=0
         )
 
-        trainset = data.build_full_trainset()
+        self.user_item_matrix = user_item_matrix
 
-        print("Training collaborative filtering model...")
+        print("Training SVD model...")
 
-        self.model = SVD()
-
-        self.model.fit(trainset)
+        self.svd = TruncatedSVD(n_components=20, random_state=42)
+        self.user_factors = self.svd.fit_transform(user_item_matrix)
 
         print("Collaborative filtering ready.")
 
-    def predict(self, user_id, product_id):
+    def recommend(self, user_id, top_n=10):
 
-        prediction = self.model.predict(user_id, product_id)
+        if user_id not in self.user_item_matrix.index:
+            return []
 
-        return prediction.est
+        user_index = list(self.user_item_matrix.index).index(user_id)
+
+        scores = self.user_factors[user_index]
+
+        item_scores = self.svd.inverse_transform(scores.reshape(1, -1))[0]
+
+        recommendations = pd.Series(item_scores, index=self.user_item_matrix.columns)
+
+        return recommendations.sort_values(ascending=False).head(top_n).index.tolist()
